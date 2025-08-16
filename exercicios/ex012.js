68a46de2451ed172bfcd76c5ecb26587aee1a70e
Luiz <henrique10v20@gmail.com>
2025-08-15 22:52:22 -0300

    Correção ex012 e Criação ex013

--- exercicios/ex012.js
+++ exercicios/ex012.js
@@ -1,6 +1,6 @@
 var agora = new Date()
-var hora = agora.getHours
-console.log(`Agora são exatamente ${horas} horas.`)
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
   console.log('Bom dia!')
 } else if (hora <= 18) {
